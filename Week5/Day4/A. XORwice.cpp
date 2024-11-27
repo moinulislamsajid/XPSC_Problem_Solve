ed19c5eb7f30100290eb3c4c8a5bb21a4ad8f3ed
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2024-11-27 23:19:01 +0600

    Add files via upload

---
+++ Week5/Day4/A. XORwice.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        long long int a,b,XOR;
+        cin>>a>>b;
+
+        XOR=a^b;
+        cout<<XOR<<endl;
+    }
+}
---
+++ Week5/Day4/Dull Operation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+
+    int t;
+    cin>>t;
+
+    cout<<(char)(64+t)<<" "<<(char)(96+t)<<endl;
+
+    while(t--)
+    {
+        long long int n;
+        cin>>n;
+
+        cout<<n<<" "<<n-1<<endl;
+    }
+
+
+}
