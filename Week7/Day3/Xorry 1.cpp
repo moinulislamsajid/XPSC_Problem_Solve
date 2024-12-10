f9eb15f85e4bfefd62c1670416a642ad807d1fdb
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2024-12-10 22:56:18 +0600

    Update Xorry 1.cpp

--- Week7/Day3/Xorry 1.cpp
+++ Week7/Day3/Xorry 1.cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a = 1;
+        int b = 0;
+        int cnt = 0;
+        while (a*2<=n)
+        {
+            a = a*2;
+            cnt++;
+        }
+        for (int i = cnt - 1; i >= 0; i--)
+        {
+            if ((n&1<<i))
+            {
+                b = b | (1<<i);
+            }
+        }
+        cout<<b<<" "<<a<<endl;
+    }
+
+    return 0;
+}
+
 
