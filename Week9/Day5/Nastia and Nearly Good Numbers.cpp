5ccabf484378e5bf9610c485f4732ce51d464aa6
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2024-12-26 22:06:20 +0600

    Add files via upload

---
+++ Week9/Day5/Nastia and Nearly Good Numbers.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        long long int a,b;
+        cin>>a>>b;
+
+        if(b==1)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+            cout<<a<<' '<<a*(long long)b<<' '<<a*(long long)(b+1)<<endl;
+        }
+    }
+}
