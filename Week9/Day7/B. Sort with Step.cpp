46ccea7404db9da0e7fb5e212b79b9d6c1d257c9
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2024-12-28 18:45:26 +0600

    Update B. Sort with Step.cpp

--- Week9/Day7/B. Sort with Step.cpp
+++ Week9/Day7/B. Sort with Step.cpp
@@ -1,1 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+
+        vector<int> v(n);
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+            v[i]--;
+        }
+
+        int cnt = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(v[i]%k != i%k)
+            {
+                cnt++;
+            }
+        }
+
+        if(cnt==0)
+        {
+            cout<<0<<endl;
+        }
+        else if(cnt==2)
+        {
+            cout<<1<<endl;
+        }
+        else
+        {
+            cout<<-1<<endl;
+        }
+    }
+}
 
