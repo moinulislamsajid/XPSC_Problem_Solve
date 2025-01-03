c6f64aec56e4c209ff1dbfb69f82c842c1a17c42
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-01-03 22:49:47 +0600

    Update collection.cpp

--- Week10/Day6/collection.cpp
+++ Week10/Day6/collection.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxN = 2e5 + 1;
+
+int N, x, cnt, pos[maxN];
+
+int main()
+{
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> x;
+        pos[x] = i;
+    }
+
+    cnt = 1;
+    for (int i = 2; i <= N; i++)
+    {
+        if (pos[i - 1] > pos[i])
+        {
+            cnt++;
+        }
+    }
+    cout << cnt << '\n';
+}
+
 
