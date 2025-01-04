ae71e5ae0452eac1702c5e23f40dc1abfdc2893f
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-01-04 22:26:54 +0600

    Update tower.cpp

--- Week10/Day7/tower.cpp
+++ Week10/Day7/tower.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> towers;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        auto it = upper_bound(towers.begin(), towers.end(), x);
+        if (it == towers.end())
+        {
+            towers.push_back(x);
+        }
+        else
+        {
+            *it = x;
+        }
+    }
+
+    cout << towers.size() << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
+
 
