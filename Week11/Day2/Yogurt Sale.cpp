1a10138b554a93f88e3db380de1dc3e8a6fdd487
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-01-12 18:31:31 +0600

    Update Yogurt Sale.cpp

--- Week11/Day2/Yogurt Sale.cpp
+++ Week11/Day2/Yogurt Sale.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    ll n, a, b;
+    cin >> n >> a >> b;
+    ll ans = n * a;
+    if (b < 2 * a)
+    {
+        ans = (n / 2) * b + (n % 2) * a;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
 
