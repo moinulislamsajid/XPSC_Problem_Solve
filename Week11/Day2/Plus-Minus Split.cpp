a0f22dfd6b2d1460303bf02f043e95c9c7f39144
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-01-12 18:30:37 +0600

    Update Plus-Minus Split.cpp

--- Week11/Day2/Plus-Minus Split.cpp
+++ Week11/Day2/Plus-Minus Split.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int n;
+string s;
+
+int main()
+{
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        cin >> s;
+        int sm = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '+')
+            {
+                sm++;
+            }
+            else
+            {
+                sm--;
+            }
+        }
+        cout << abs(sm) << endl;
+    }
+    return 0;
+}
 
