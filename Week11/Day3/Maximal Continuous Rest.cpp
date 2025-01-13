9ac181022228aaf959f1af4355451712d21e5a8e
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-01-13 16:15:30 +0600

    Update Maximal Continuous Rest.cpp

--- Week11/Day3/Maximal Continuous Rest.cpp
+++ Week11/Day3/Maximal Continuous Rest.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int result = 0;
+    int len = 0;
+    for (int i = 0; i < 2 * n; i++)
+    {
+        if (a[i % n] == 1)
+        {
+            len++;
+            result = max(result, len);
+        }
+        else
+        {
+            len = 0;
+        }
+    }
+    cout << result << endl;
+}
 
